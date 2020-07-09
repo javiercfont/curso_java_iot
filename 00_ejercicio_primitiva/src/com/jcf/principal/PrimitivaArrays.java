ffa292f33b35ebe6160163b249e9c0be48f23868
Javier <Javier@DESKTOP-V41T3L2>
2020-07-09 11:50:18 +0200

    9-7-2020 11:50. Se realiza el ejercicio usando un array de enteros

--- 00_ejercicio_primitiva/src/com/jcf/principal/PrimitivaArrays.java
+++ 00_ejercicio_primitiva/src/com/jcf/principal/PrimitivaArrays.java
@@ -1,12 +1,67 @@
 package com.jcf.principal;
 
+import java.util.Arrays;
+
 public class PrimitivaArrays {
+	
+	// Busca si existe el número en el array
+	
+	public static boolean buscar(int[] datos, int num) {
+		
+		for (int n : datos) {
+			
+			if (num == n) {
+				
+				return true;
+			}
+		}
+		
+		return false;
+	}
 
 	public static void main(String[] args) {
 		
-		final int maxNumero = 49;
+		final int maxNumeros = 49;
 		final int maxArray = 6;
+		final int maxReintegro = 6;
+		
+		int numero = 0;
+		int index = 0;
+		
+		int[] boleto = new int[maxArray];
+		
+		// Genera números hasta alcanzar el total de números por apuesta
+		
+		do {
+			
+			// Genera un número en los límites establecidos
+			
+			numero = (int) ((Math.random() * maxNumeros) + 1);
+			
+			/* Comprueba si el boleto contiene el número generado.
+			   Si no existe lo añade */
+			
+			for (int dummy : boleto) {
+				
+				if (!buscar(boleto, numero)) {
+				
+					boleto[index++] = numero;
+					break;
+				}
+			}
+			
+		} while (index < maxArray);
+		
+		// Ordena el boleto
+		
+		Arrays.sort(boleto);
+		
+		// Muestra el resultado
+		
+		for (int n : boleto) {
+			
+			System.out.print(n + ",");
+		}
 
 	}
-
 }
